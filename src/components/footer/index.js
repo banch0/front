ec33b2062d252c927e9caae969d99398a84dad03
= <iooftob@gmail.com>
2019-11-05 07:55:45 +0500

    added some front

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
+
+const Footer = () => (
+	<footer class={style.footer}>
+		<h1>Preact App</h1>
+	</footer>
+);
+
+export default Footer;
